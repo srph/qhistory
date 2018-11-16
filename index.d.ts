eb4895d8cdd3b1c361bc755900ab5309a62a6fbb
Kier Borromeo <seraphipod@gmail.com>
2018-11-16 20:55:07 +0800

    Make it work for idk what version

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { History } from 'history';
 
-type Stringify = (query: any) => string;
-type Parse = (query: string) => any;
-const historyWithQuery: (history: History, stringify: Stringify, parse: Parse) => History;
+declare type Stringify = (query: any) => string;
+declare type Parse = (query: string) => any;
+declare const historyWithQuery: (history: History, stringify: Stringify, parse: Parse) => History;
 
 export default historyWithQuery;
